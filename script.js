00a2768e32e3f2cb09cd3dd38be645cb6c1facc2
Riley <riley.childs@yahoo.com>
2023-04-27 15:46:29 -0400

    Created DOM manipulation functionality for page selectors

--- script.js
+++ script.js
@@ -28,13 +28,58 @@ navToggle.container.onclick = () => {
   navToggle.toggle();
 };
 
+function getSelector(type) {
+  const selector = document.getElementsByClassName(`${type}__selector`)[0];
+  return selector;
+}
+
+function createSelection(type, text) {
+  const btn = document.createElement("button");
+  btn.className = `${type}__selector--btn`;
+  btn.innerText = `${text}`;
+  return btn;
+}
+
+const selectors = {
+  destinations: getSelector("destination"),
+  crew: getSelector("crew"),
+  technology: getSelector("technology"),
+  loadSelections: (type, text) => {},
+};
+
 async function getData() {
   const resonse = await fetch("data.json");
   const data = await resonse.json();
-  return data
+  return data;
 }
 
 getData().then((data) => {
-  const {destinations, crew, technology} = data
-  
+  const { destinations, crew, technology } = data;
+
+
+  if (selectors.destinations) {
+    for (let x in destinations) {
+      const selection = destinations[x].name.toUpperCase();
+      const destination = createSelection("destination", selection);
+      selectors.destinations.append(destination);
+    }
+  }
+
+  if (selectors.crew) {
+    for (let x in crew) {
+      const selection = "";
+      const crewSelection = createSelection("crew", selection);
+      selectors.crew.append(crewSelection);
+    }
+  }
+
+  if (selectors.technology) {
+    for (let x in technology) {
+      const selection = parseInt(x) + 1;
+      const technology = createSelection("technology", selection);
+      selectors.technology.append(technology);
+    }
+  }
+
+
 });
